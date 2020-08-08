a99cc3bebcb9d8dbccab445ecbfa23e2ee07f586
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-08-07 21:23:01 -0300

    Versão final 1.0 do projeto

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Image, Text, TouchableOpacity, Button } from 'react-native'
 import styles from './styles'
 import { useNavigation } from '@react-navigation/native'
@@ -7,9 +7,18 @@ import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png'
 import giveClassIcon from '../../assets/images/icons/give-classes.png'
 import heartIcon from '../../assets/images/icons/heart.png'
+import api from '../../services/api'
 
 function Landing() {
   const {navigate} = useNavigation()
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const {total} = response.data
+      setTotalConnections(total)
+    })
+  }, [])
 
   function handleNavigateToGiveClassesPage(){
     navigate('GiveClasses')
@@ -47,7 +56,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
